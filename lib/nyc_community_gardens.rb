d8db4e06528c7af92ff909f691acf250859bd934
Claudia McCormack <mccormack.chc@gmail.com>
2016-09-13 21:10:56 -0400

    added .find_by and .where methods to CommunityGarden class

--- lib/nyc_community_gardens.rb
+++ lib/nyc_community_gardens.rb
@@ -13,7 +13,7 @@ module NycCommunityGardens
     end
 
     def self.all
-      gardens_array = Unirest.get('https://data.cityofnewyork.us/resource/yes4-7zbb.json').body
+      gardens_array = Unirest.get("https://data.cityofnewyork.us/resource/yes4-7zbb.json").body
       gardens = []
       gardens_array.each do |garden|
         gardens << garden
@@ -21,6 +21,22 @@ module NycCommunityGardens
       gardens
     end
 
+    def self.where
+      key = search_term.keys.to_s
+      value = search_term.values
+      gardens_array = Unirest.get("https://data.cityofnewyork.us/resource/yes4-7zbb.json?#{key}=#{value}").body
+      gardens = []
+      gardens_array.each do |garden|
+        gardens << garden
+      end
+    end
+
+    def self.find_by(search_term)
+      key = search_term.keys.first.to_s
+      value = search_term.values.first
+      garden = Unirest.get("https://data.cityofnewyork.us/resource/yes4-7zbb.json?#{key}=#{value}").body.first
+      CommunityGarden.new(garden)
+    end
 
   end
 end
